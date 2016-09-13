1a26d92105c12fa8bc61d4028a2c56e168754c23
Anna Doubkova <anna.doubkova@red-badger.com>
2016-09-13 17:36:13 +0100

    WIP shift towards serverless v1-RC1

--- index.js
+++ index.js
@@ -1,30 +1,89 @@
-'use strict';
 const path = require('path');
 const webpack = require('webpack');
 const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs-extra'));
+const mkdirs = Promise.promisify(require('fs-extra').mkdirs);
 
 const getConfig = require('./lib/getConfig');
 const getExternalsFromStats = require('./lib/getExternalsFromStats');
 const copyModules = require('./lib/copyModules');
 
-
 function runWebpack(config) {
-  return new Promise((resolve, reject) => {
+  console.log('running');
+  return new Promise((resolve, reject) =>  {
     webpack(config).run((err, stats) => {
       if (err) {
+        console.log('webpack error');
         return reject(err);
       }
-      resolve(stats);
+      console.log('done with packing');
+      return resolve(stats);
     });
   });
 }
 
-module.exports = function getPlugin(S) {
-  const SCli = require(S.getServerlessPath('utils/cli'));
+module.exports = class ServerlessWebpack {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
 
-  function logStats(stats) {
-    SCli.log(stats.toString({
+    this.hooks = {
+      'before:deploy:compileFunctions': this.optimize.bind(this)
+    };
+    this.logStats.bind(this);
+  }
+
+  optimize() {
+    // Validate: Check Serverless version
+    if (parseInt(this.serverless.version.split('.')[0], 10) < 1) {
+      this.serverless.cli.log('WARNING: This version of the Serverless Optimizer Plugin ' +
+        'will not work with a version of Serverless that is less than v1');
+    }
+    const { runtime } = this.serverless.service.provider;
+    if (runtime === 'nodejs' || runtime === 'nodejs4.3') {
+      const currentDir = this.serverless.config.servicePath;
+      const config = getConfig(currentDir, this.serverless.service.provider.custom);
+
+      if (config.webpackConfig) {
+        const pathDist = this.options.pathDist || '.serverless';
+        const optimizedPath = path.join(pathDist, 'optimized');
+        const optimizedModulesPath = path.join(optimizedPath, 'node_modules');
+
+        const webpackConfig = Object.assign({}, config.webpackConfig);
+
+        const handlerFileName = 'handler.js';
+        const handlerEntryPath = `./${handlerFileName}`;
+        // override entry and output
+        webpackConfig.context = path.dirname(handlerEntryPath);
+        if (Array.isArray(webpackConfig.entry)) {
+          webpackConfig.entry.push(handlerEntryPath);
+        } else {
+          webpackConfig.entry = handlerEntryPath;
+        }
+
+        webpackConfig.output = {
+          libraryTarget: 'commonjs',
+          path: optimizedPath,
+          filename: handlerFileName
+        };
+
+        return mkdirs(optimizedModulesPath)
+          .then(() => runWebpack(webpackConfig))
+          .then((stats) => {
+            this.logStats(stats);
+            const externals = getExternalsFromStats(stats);
+            return copyModules(currentDir, externals, optimizedModulesPath);
+          })
+          .then(() => {
+            this.options.pathDist = optimizedPath; // eslint-disable-line
+            return this;
+          });
+      }
+    }
+    return null;
+  }
+
+  logStats(stats) {
+    this.serverless.cli.log(stats.toString({
       colors: true,
       hash: false,
       version: false,
@@ -32,87 +91,4 @@ module.exports = function getPlugin(S) {
       children: false
     }));
   }
-
-  class ServerlessWebpack extends S.classes.Plugin {
-
-    static getName() {
-      return `com.serverless.${ServerlessWebpack.name}`;
-    }
-
-    registerHooks() {
-      S.addHook(this.optimize.bind(this), {
-        action: 'codeDeployLambda',
-        event: 'pre'
-      });
-
-      return Promise.resolve();
-    }
-
-    optimize(evt) {
-      // Validate: Check Serverless version
-      if (parseInt(S._version.split('.')[1], 10) < 5) {
-        SCli.log('WARNING: This version of the Serverless Optimizer Plugin ' +
-          'will not work with a version of Serverless that is less than v0.5');
-      }
-
-      // Get function
-      const project = S.getProject();
-      const func = project.getFunction(evt.options.name);
-
-      if (func.runtime === 'nodejs' || func.runtime === 'nodejs4.3') {
-        const projectPath = S.config.projectPath;
-        const config = getConfig(
-          projectPath,
-          project.toObjectPopulated(evt.options),
-          func.toObjectPopulated(evt.options)
-        );
-
-        if (config.webpackConfig) {
-          const pathDist = evt.options.pathDist;
-          const optimizedPath = path.join(pathDist, 'optimized');
-          const optimizedModulesPath = path.join(optimizedPath, 'node_modules');
-
-          const webpackConfig = Object.assign({}, config.webpackConfig);
-          const handlerName = func.getHandler().split('.')[0];
-          const handlerFileName = `${handlerName}.${config.handlerExt}`;
-          const handlerEntryPath = `./${handlerFileName}`;
-
-          // override entry and output
-          webpackConfig.context = path.dirname(func.getFilePath());
-          if (Array.isArray(webpackConfig.entry)) {
-            webpackConfig.entry.push(handlerEntryPath);
-          } else {
-            webpackConfig.entry = handlerEntryPath;
-          }
-          webpackConfig.output = {
-            libraryTarget: 'commonjs',
-            path: optimizedPath,
-            filename: handlerFileName
-          };
-
-          // copy generated handler so we can build directly from the source directory
-          const generatedHandler = path.join(webpackConfig.context, handlerFileName);
-
-          return fs.copyAsync(path.join(pathDist, handlerFileName), generatedHandler)
-            .then(() => fs.mkdirsAsync(optimizedModulesPath))
-            .then(() => runWebpack(webpackConfig))
-            .then((stats) => {
-              logStats(stats);
-              const externals = getExternalsFromStats(stats);
-              return copyModules(projectPath, externals, optimizedModulesPath);
-            })
-            .then(() => {
-              evt.options.pathDist = optimizedPath; // eslint-disable-line
-              return evt;
-            })
-            // delete generated handler we copied above
-            .finally(() => fs.removeAsync(generatedHandler));
-        }
-      }
-
-      return Promise.resolve(evt);
-    }
-  }
-
-  return ServerlessWebpack;
 };
